b89472147181ed2e735183127e093f6d52276466
Akos Pasztor <mail@akospasztor.com>
2025-09-19 15:32:25 +0200

    Fix the plugin name in settings.ts
    The plugin name must reflect the name in package.json

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'CreateCeilingFanPlatform';
 /**
  * This must match the name of your plugin as defined the package.json `name` property
  */
-export const PLUGIN_NAME = 'homebridge-create-ceiling-fan';
+export const PLUGIN_NAME = '@akospasztor/homebridge-create-ceiling-fan';
